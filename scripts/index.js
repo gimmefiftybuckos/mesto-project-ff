5aef31b5598ddecf80270f1753ea1d764d048164
Dima Birichev <dbirichev@gmail.com>
2023-11-18 23:05:06 +0300

    Первая подача

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,29 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardList = document.querySelector('.places__list')
+
+function addCard (card) {
+    const cardTemplate = document.querySelector('#card-template').content 
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true)
+    const cardImage = cardElement.querySelector('.card__image')
+    const removeButton = cardElement.querySelector('.card__delete-button')
+
+    cardElement.querySelector('.card__title').textContent = card.name
+    cardImage.setAttribute('src', card.link)
+    cardImage.setAttribute('alt', `Фотография: ${card.name}`)
+
+    removeButton.addEventListener('click', removeCard)
+    
+    cardList.append(cardElement)
+}
+
+for (card of initialCards) {
+    addCard(card)
+}
+
+function removeCard (event) {
+    cardElement = event.target.closest('.card')
+    cardElement.remove()
+}
